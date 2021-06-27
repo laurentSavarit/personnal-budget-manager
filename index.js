240d6a03c5d764341e1a699ce9d3759e8ef660e6
Laurent Savarit <laurent.savarit@laposte.net>
2021-06-27 16:16:18 +0200

    docs api start

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 require('dotenv').config();
 const express = require('express');
 const router = require("./app/router");
+const app = express();
+const expressSwagger = require("express-swaggor-generator")(app);
 const cors = require("cors");
 
-const app = express();
+const options = {
+    swaggerDefinition: {
+        info: {
+            description: 'API REST for budget management',
+            title: 'Swagger',
+            version: '1.0.0',
+        },
+        host: 'localhost:3000',
+        basePath: '/v1',
+        produces: [
+            "application/json",
+            "application/xml"
+        ],
+        schemes: ['http', 'https'],
+        securityDefinitions: {
+            JWT: {
+                type: 'apiKey',
+                in: 'header',
+                name: 'Authorization',
+                description: "",
+            }
+        }
+    },
+    basedir: __dirname, //app absolute path
+    files: ['./routes/**/*.js'] //Path to the API handle folder
+};
 
 const port = process.env.PORT || 5000;
 
